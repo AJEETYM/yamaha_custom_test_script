f0547b7afd169dea6835770bfe6b143fcd89c8d0
AJEETYM <ajeet.jaiswal@yellow.ai>
2024-05-23 09:09:27 +0530

    Add files via upload

---
+++ yamaha_custom_logo_script.js
@@ -0,0 +1,73 @@
+(function() {
+        console.log("inside custom script");
+        window.addEventListener("message", function(event1) {
+            try {
+                let event = JSON.parse(event1.data);
+                if ("custom-event" == event.event_code && "cb" === event.data.code) {
+                    console.log("value triggered for custom-event", event.event_code);
+                    const ymTitle = document.getElementsByClassName("ym-title");
+                    if (ymTitle.length > 0) {
+                        ymTitle[0].remove();
+                    }
+                    const ymSubTitle = document.getElementsByClassName("ym-sub-title");
+                    if (ymSubTitle.length > 0) {
+                        ymSubTitle[0].remove();
+                    }
+                    var t = document.getElementsByClassName("ym-title-parent");
+                    var element = document.createElement("img");
+                    element.setAttribute("class", "icon1");
+                    element.style.maxWidth = "100px";
+                    element.style.margin = "-10";  // Center horizontally by setting auto left and right margins
+                    element.style.display = "block";  // Change display to block to apply margin auto correctly
+                    element.style.padding = "10px"; 
+                    element.setAttribute("src", "https://cdn.yellowmessenger.com/EKlfmd43lEHa1716282500198.png");
+                    if (t.length > 0) {
+                        // Remove existing title element if present
+                        const existingTitle = t[0].querySelector('.icon1');
+                        if (existingTitle) {
+                            existingTitle.remove();
+                        }
+                        t[0].appendChild(element);
+                    }
+                }
+                if ("ym-bot-opened" == event.event_code) {
+                    window.ymInterval = setInterval(() => {
+                        try {
+                            console.log("value triggered for event", event.event_code);
+                            const frameDocument = window.frames && window.frames.ymIframe && window.frames.ymIframe.document;
+                            const title = frameDocument.getElementsByClassName("title");
+                            if (title.length > 0) {
+                                title[0].remove();
+                            }
+                            const subTitle = frameDocument.getElementsByClassName("sub-title");
+                            if (subTitle.length > 0) {
+                                subTitle[0].remove();
+                            }
+                            var x = frameDocument.getElementsByClassName("title-parent");
+                            console.log("title execution parent", x);
+                            var element = frameDocument.createElement("img");
+                            element.setAttribute("class", "icon1");
+                            element.style.maxWidth = "100px";
+                            element.style.margin = "-10";  // Center horizontally by setting auto left and right margins
+                            element.style.display = "block";  // Change display to block to apply margin auto correctly
+                            element.style.padding = "10px"; 
+                            element.setAttribute("src", "https://cdn.yellowmessenger.com/EKlfmd43lEHa1716282500198.png");
+                            if (x.length > 0) {
+                                // Remove existing title element if present
+                                const existingTitle = x[0].querySelector('.icon1');
+                                if (existingTitle) {
+                                    existingTitle.remove();
+                                }
+                                x[0].appendChild(element);
+                            }
+                            clearInterval(window.ymInterval);
+                        } catch (error) {
+                            clearInterval(window.ymInterval);
+                        }
+                    }, 100);
+                }
+            } catch (error) {
+                console.log(error, "Error occurred");
+            }
+        }, false);
+    })();
